08426dbd886677bf75503152c05b303daf4acb52
Asus <Asus@DESKTOP-F2RLVQ4>
2024-09-21 20:44:47 +0300

    Добавьте файлы проекта.

---
+++ style/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace MyWPFApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("кнопка нажата!");
+        }
+    }
+}
